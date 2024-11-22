f3ba099b7e78d893a792f5fb80f44ba700bfaf02
Seixas <carlosseixasof@gmail.com>
2024-11-22 09:56:57 -0300

    Controller GYM com Rotas e Create

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environmentMatchGlobs: [['./src/http/controllers/tests/**', 'prisma']],
+    environmentMatchGlobs: [['./src/http/controllers/**', 'prisma']],
   },
 })
